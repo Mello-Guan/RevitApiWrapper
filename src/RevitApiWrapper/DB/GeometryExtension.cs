c02c6ec61a833a0fcd1f3a5aa6673e40e22ed534
Mello <Mello@qq.com>
2022-08-03 17:55:27 +0800

    第一次提交

--- src/RevitApiWrapper/DB/GeometryExtension.cs
+++ src/RevitApiWrapper/DB/GeometryExtension.cs
@@ -272,5 +272,241 @@ namespace RevitApiWrapper.DB
         }
 
 
+
+
+
+        /// <summary>
+        /// 两点形成直线在已知方向上的投影直线
+        /// </summary>
+        /// <param name="l">两点形成直线</param>
+        /// <param name="Ori">已知方向向量</param>
+        /// <returns></returns>
+        public static Line ProjectionLineOnOri(this Line l, XYZ Ori)
+        {
+            XYZ p1 = l.GetEndPoint(0);
+            XYZ p2 = l.GetEndPoint(1);
+            Line OriLine = Line.CreateUnbound(p1, Ori);
+            XYZ p1_ = OriLine.Project(p1).XYZPoint;
+            XYZ p2_ = OriLine.Project(p2).XYZPoint;
+            if (p1_.DistanceTo(p2) < 1e-6) { return null; }
+            Line rL = Line.CreateBound(p1_, p2_);
+            return rL;
+        }
+
+
+        /// <summary>
+        /// 返回一个点在平面上的投影点坐标
+        /// </summary>
+        /// <param name="xyz">平面外任一点坐标</param>
+        /// <param name="planeP">平面上的任一点坐标</param>
+        /// <param name="planeNormal">平面的法向量</param>
+        /// <returns></returns>
+        public static XYZ GetPointOnPlane(this XYZ xyz, XYZ planeP, XYZ planeNormal)
+        {
+            double x = 0;
+            double y = 0;
+            double z = 0;
+
+            x = (planeNormal.X * planeNormal.X * planeP.X + planeNormal.Y * planeNormal.Y * xyz.X - planeNormal.Y * xyz.Y * planeNormal.X + planeNormal.Y * planeP.Y * planeNormal.X + planeNormal.Z * planeNormal.Z * xyz.X - planeNormal.Z * xyz.Z * planeNormal.X + planeNormal.Z * planeP.Z * planeNormal.X) / (planeNormal.X * planeNormal.X + planeNormal.Y * planeNormal.Y + planeNormal.Z * planeNormal.Z);
+            y = (planeNormal.Y * planeNormal.Z * planeP.Z + planeNormal.Z * planeNormal.Z * xyz.Y - planeNormal.Y * planeNormal.Z * xyz.Z + planeNormal.Y * planeNormal.X * planeP.X + planeNormal.X * planeNormal.X * xyz.Y - planeNormal.X * planeNormal.Y * xyz.X + planeNormal.Y * planeNormal.Y * planeP.Y) / (planeNormal.X * planeNormal.X + planeNormal.Y * planeNormal.Y + planeNormal.Z * planeNormal.Z);
+            z = (planeNormal.X * planeP.X * planeNormal.Z + planeNormal.X * planeNormal.X * xyz.Z - planeNormal.X * xyz.X * planeNormal.Z + planeNormal.Y * planeP.Y * planeNormal.Z + planeNormal.Y * planeNormal.Y * xyz.Z - planeNormal.Y * xyz.Y * planeNormal.Z + planeNormal.Z * planeNormal.Z * planeP.Z) / (planeNormal.X * planeNormal.X + planeNormal.Y * planeNormal.Y + planeNormal.Z * planeNormal.Z);
+
+            return new XYZ(x, y, z);
+        }
+
+
+
+        /// <summary>
+        /// 返回直线在平面上的投影直线
+        /// </summary>
+        /// <param name="line">平面外的一条直线</param>
+        /// <param name="planeP">平面上的任一点坐标</param>
+        /// <param name="planeNormal">平面的法向量</param>
+        /// <returns></returns>
+        public static Line GetLineOnPlane(this Line line, XYZ planeP, XYZ planeNormal)
+        {
+            if (line.IsBound)
+            {
+                Line newl = Line.CreateBound(GetPointOnPlane(line.GetEndPoint(0), planeP, planeNormal), GetPointOnPlane(line.GetEndPoint(1), planeP, planeNormal));
+                return newl;
+            }
+            else
+            {
+                XYZ newOri = (GetPointOnPlane(line.Origin + line.Direction * 100, planeP, planeNormal) - GetPointOnPlane(line.Origin, planeP, planeNormal)).Normalize();
+                Line newl = Line.CreateUnbound(GetPointOnPlane(line.Origin, planeP, planeNormal), newOri);
+                return newl;
+            }
+        }
+
+        /// <summary>
+        /// 返回两条直线交点方法
+        /// </summary>
+        /// <param name="line_1">第一条直线</param>
+        /// <param name="line_2">第二条直线</param>
+        /// <returns></returns>
+        public static XYZ Line_IEresult(this Line line_1, Line line_2)
+        {
+            XYZ xyz = null;
+            IntersectionResultArray result;
+            SetComparisonResult scr = line_1.Intersect(line_2, out result);
+            if (scr == SetComparisonResult.Overlap)//不重合
+            {
+                if (SetComparisonResult.Disjoint != scr)//相交
+                {
+                    xyz = result.get_Item(0).XYZPoint;
+                }
+            }
+            return xyz;
+        }
+
+
+        /// <summary>
+        /// 返回两个向量定向旋转的夹角(弧度值)，第二个向量为基准向量, rotateOri 为 ViewDirection 为顺时针，rotateOri为 ViewDirection.Negate() 为逆时针
+        /// </summary>
+        /// <param name="a">第一个向量</param>
+        /// <param name="b">第二个向量（基准向量）</param>
+        /// <param name="rotateOri">旋转方向，ViewDirection 为顺时针，ViewDirection.Negate() 为逆时针</param>
+        /// <returns></returns>
+        public static double SignedAngleBetween(this XYZ a, XYZ b, XYZ rotateOri)
+        {
+            double angle = a.AngleTo(b);
+
+            int sign = Math.Sign(rotateOri.DotProduct(a.CrossProduct(b)));
+
+            double signed_angle = angle * sign;
+
+            if (signed_angle == 0)
+            {
+                if (a.IsAlmostEqualTo(b, 1e-6)) { signed_angle = 0; }
+                else { signed_angle = Math.PI; }
+            }
+
+            else if (signed_angle < 0)
+            {
+                signed_angle = Math.PI * 2 + signed_angle;
+            }
+
+            return signed_angle;
+        }
+
+
+
+
+
+        /// <summary>
+        /// 获得线与面的交点
+        /// </summary>
+        /// <param name="face"></param>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        public static XYZ CurveIntersectFace(this Face face, Line line)
+        {
+
+            XYZ returnXYZ = null;
+
+            IntersectionResultArray ira = new IntersectionResultArray();
+
+            SetComparisonResult scr = face.Intersect(line, out ira);
+
+            if (scr != SetComparisonResult.Disjoint)
+            {
+
+                if (ira == null)
+                {
+                    return returnXYZ;
+                }
+
+                if (!ira.IsEmpty)
+                {
+                    returnXYZ = ira.get_Item(0).XYZPoint;
+                }
+            }
+
+            return returnXYZ;
+        }
+
+
+        /// <summary>
+        /// 得到一个点沿着一个直线轴旋转一定角度后的点坐标
+        /// </summary>
+        /// <param name="axis">直线轴：可以为射线</param>
+        /// <param name="angle">旋转角度值</param>
+        /// <param name="p">已知点坐标</param>
+        /// <returns></returns>
+        public static XYZ GetPointRotateAlongAxis(this Line axis, double angle, XYZ p)
+        {
+            XYZ rp = p;
+
+            Line axis2 = axis;
+            if (!axis.IsBound) { axis2 = Line.CreateBound(axis.Origin, axis.Origin + 1000 * axis.Direction); }
+
+            XYZ v = (p - axis2.GetEndPoint(0)).Normalize();
+
+            XYZ u = (axis2.GetEndPoint(1) - axis2.GetEndPoint(0)).Normalize();
+
+            double a = AngleToRadian(angle);
+
+            XYZ vv = Math.Cos(a) * v + (1 - Math.Cos(a)) * (u.DotProduct(v)) * u + Math.Sin(a) * (u.CrossProduct(v));
+
+            rp = axis2.GetEndPoint(0) + vv * (p.DistanceTo(axis2.GetEndPoint(0)));
+
+            return rp;
+        }
+
+
+
+
+        /// <summary>
+        /// 判断点是否在区域内(区域边界线均为直线)
+        /// </summary>
+        /// <param name="array">区域边界线集合</param>
+        /// <param name="point">需要判定的点</param>
+        /// <param name="view">需要判定的视图</param>
+        /// <returns></returns>
+        public static bool PointInRegion(this CurveArray array, XYZ point, Autodesk.Revit.DB.View view)
+        {
+            XYZ xyz = GetPointOnPlane(point, view.Origin, view.ViewDirection);
+            Line line = Line.CreateBound(xyz + view.RightDirection * 1e6, xyz - view.RightDirection * 1e6);
+
+            List<XYZ> xyzlist = new List<XYZ>();
+            xyzlist.Add(xyz);
+
+            foreach (Curve curve in array)
+            {
+                Line line_1 = Line.CreateBound(GetPointOnPlane(curve.GetEndPoint(0), view.Origin, view.ViewDirection), GetPointOnPlane(curve.GetEndPoint(1), view.Origin, view.ViewDirection));
+                XYZ xyz_1 = Line_IEresult(line, line_1);
+                if (xyz_1 != null)
+                {
+                    bool buer = false;
+
+                    if (xyzlist.Where(o => o.IsAlmostEqualTo(xyz_1)).Count() > 0) { buer = true; }
+
+                    if (buer == false)
+                    {
+                        xyzlist.Add(xyz_1);
+                    }
+
+                }
+            }
+
+            List<XYZ> listing = OrderPointsOnOri(xyzlist, view.RightDirection);
+            int d = 0;
+            for (int c = 0; c < listing.Count; c++)
+            {
+                if (listing[c].IsAlmostEqualTo(xyz))
+                {
+                    d = c;
+                    break;
+                }
+            }
+            if (d % 2 == 1 && (listing.Count - 1 - d) % 2 == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
     }
 }
---
+++ src/RevitApiWrapper/DB/ViewExtension.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RevitApiWrapper.DB
+{
+    public static class ViewExtension
+    {
+
+        public static enum CheckViewSet
+        {
+            IsViewPlan,
+            IsView3D,
+            IsViewSection,
+            NoView3D
+        }
+
+
+        /// <summary>
+        /// 判断当前视图
+        /// </summary>
+        /// <param name="checkViewSet">判断设置</param>
+        /// <param name="doc">文档</param>
+        /// <param name="falseTip">判断为否时候的信息提示</param>
+        /// <returns></returns>
+        public static bool CheckView(this CheckViewSet checkViewSet,Document doc, string falseTip)
+        {
+            bool bur = false;
+            Autodesk.Revit.DB.View view = doc.ActiveView;
+
+            switch (checkViewSet)
+            {
+                case CheckViewSet.IsView3D:
+                    if (view is View3D) { bur = true; }
+                    else { bur = false; }
+                    break;
+
+                case CheckViewSet.IsViewPlan:
+                    if (view is ViewPlan) { bur = true; }
+                    else { bur = false; }
+                    break;
+
+                case CheckViewSet.IsViewSection:
+                    if (view is ViewSection) { bur = true; }
+                    else { bur = false; }
+                    break;
+
+                case CheckViewSet.NoView3D:
+                    if (view is View3D) { bur = false; }
+                    else { bur = true; }
+                    break;
+
+                default:
+                    break;
+            }
+
+            if (bur == false)
+            {
+                MessageBox.Show(falseTip, "提示");
+            }
+
+            return bur;
+        }
+
+
+
+
+    }
+}
